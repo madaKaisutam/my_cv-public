87f431c7bc7c9672db068304e8bdc3d9f2f4819b
madaKaisutam <madaoikonip@gmail.com>
2023-02-14 21:35:35 +0100

    bootstrap, sql, navbar update

--- mojeCV/settings.py
+++ mojeCV/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,8 +77,13 @@ WSGI_APPLICATION = 'mojeCV.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME' : 'my_cv_sql',
+        'USER' : 'root',
+        'PASSWORD' : 'admin',
+        'PORT' : 3306,
+        'HOST' : '127.0.0.1'
+        # 'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
@@ -116,7 +122,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- mojeCV/urls.py
+++ mojeCV/urls.py
@@ -15,6 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
 from my_cv_main_app import views as my_cv_main_app_view
 
 urlpatterns = [
@@ -22,4 +25,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('main/', include('my_cv_main_app.urls')),
     
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
